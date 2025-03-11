81027be332403d5d29312abeb01e190e8c2e8007
CoolShivas <shivas2710cool00@gmail.com>
2025-03-11 17:36:23 +0530

    Use of Arguments while emiting an event.

--- events/extra.js
+++ events/extra.js
@@ -2,12 +2,32 @@ const EventEmit = require('events');
 
 const checking = new EventEmit();
 
-checking.addListener("greetings", () => {
-    console.log("Hemendra Pratap Singh Chouhan");
-})
+// checking.addListener("greetings", () => {
+//     console.log("Hemendra Pratap Singh Chouhan");
+// })
 
-checking.emit("greetings")
+// checking.emit("greetings")
 
 
 // // // //  Here, we are getting the Output on Terminal as :-
 // // // //  Hemendra Pratap Singh Chouhan
+
+
+
+///////************************************************************************************** */
+
+
+
+// // // We can also Pass Arguments while emiting :-
+
+
+checking.on("greeting", (userName) => {
+    console.log(`Hello, ${userName}. Hope, you are well and good.`);
+})
+
+checking.emit("greeting", "Hemendra Pratap Singh Chouhan");
+
+
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // Hello, Hemendra Pratap Singh Chouhan. Hope, you are well and good.
+
