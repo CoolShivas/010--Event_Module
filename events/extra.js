57c4c8a46838f31437c16167581891c446a80a32
CoolShivas <shivas2710cool00@gmail.com>
2025-03-11 17:42:05 +0530

    Use of Multiple Arguments while emiting an event.

--- events/extra.js
+++ events/extra.js
@@ -18,16 +18,38 @@ const checking = new EventEmit();
 
 
 
-// // // We can also Pass Arguments while emiting :-
+// // // // We can also Pass Arguments while emiting :-
 
 
-checking.on("greeting", (userName) => {
-    console.log(`Hello, ${userName}. Hope, you are well and good.`);
-})
+// checking.on("greeting", (userName) => {
+//     console.log(`Hello, ${userName}. Hope, you are well and good.`);
+// })
 
-checking.emit("greeting", "Hemendra Pratap Singh Chouhan");
+// checking.emit("greeting", "Hemendra Pratap Singh Chouhan");
 
 
-// // // // Here, we are getting the Output on Terminal as :-
-// // // // Hello, Hemendra Pratap Singh Chouhan. Hope, you are well and good.
+// // // // // Here, we are getting the Output on Terminal as :-
+// // // // // Hello, Hemendra Pratap Singh Chouhan. Hope, you are well and good.
 
+
+
+
+
+///////************************************************************************************** */
+
+
+
+checking.addListener("latest", (userName, profession) => {
+    console.log(`Name : ${userName} , Occupation : ${profession}`);
+});
+
+
+checking.emit("latest", "Hemendra Pratap Singh Chouhan", "Farming");
+
+
+
+// // // // // Here, we are getting the Output on Terminal as :-
+// // // // // Name : Hemendra Pratap Singh Chouhan , Occupation : Farming
+
+
+///////************************************************************************************** */
