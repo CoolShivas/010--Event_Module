87444e8649b363341312dfeeffc74657c73cba18
CoolShivas <shivas2710cool00@gmail.com>
2025-03-11 18:00:01 +0530

    Use of Object method to pass Multiple Arguments while emiting an event.

--- events/extra.js
+++ events/extra.js
@@ -39,17 +39,42 @@ const checking = new EventEmit();
 
 
 
-checking.addListener("latest", (userName, profession) => {
-    console.log(`Name : ${userName} , Occupation : ${profession}`);
-});
+// checking.addListener("latest", (userName, profession) => {
+//     console.log(`Name : ${userName} , Occupation : ${profession}`);
+// });
 
 
-checking.emit("latest", "Hemendra Pratap Singh Chouhan", "Farming");
+// checking.emit("latest", "Hemendra Pratap Singh Chouhan", "Farming");
 
 
 
-// // // // // Here, we are getting the Output on Terminal as :-
-// // // // // Name : Hemendra Pratap Singh Chouhan , Occupation : Farming
+// // // // // // Here, we are getting the Output on Terminal as :-
+// // // // // // Name : Hemendra Pratap Singh Chouhan , Occupation : Farming
 
 
 ///////************************************************************************************** */
+
+
+
+
+// // // // But, the Best Way to take a Single Argument as an OBJECT :-
+
+
+
+
+checking.on("exclusive", (arguMents) => {
+    console.log(
+      `Name : ${arguMents.userName}, Occupation : ${arguMents.profession}, Address : ${arguMents.address}, Mobile : ${arguMents.mobile}`
+    );
+  });
+  
+  checking.emit("exclusive", {
+    userName: "Hemendra Pratap Singh Chouhan",
+    profession: "Farming",
+    address: "New Road, India",
+    mobile: "123456789",
+  });
+
+
+ // // // // // Here, we are getting the Output on Terminal as :-
+// // // // //  Name : Hemendra Pratap Singh Chouhan, Occupation : Farming, Address : New Road, India, Mobile : 123456789
